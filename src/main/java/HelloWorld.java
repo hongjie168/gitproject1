4e36cf1d8a1aec5c8c65420349dbc05e67227bd9
hongjie168 <863303874@qq.com>
2019-04-06 23:13:50 +0800

    项目初始化提交了hello

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName HelloWorld
+ * @Description git
+ * @Author Administrator
+ * @Date 23:10 2019/4/6 0006
+ * @Version 2.1
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
