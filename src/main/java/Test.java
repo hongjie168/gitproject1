dca9484185d9834eb43485bc1d24dd18d3067ec1
hongjie168 <863303874@qq.com>
2019-04-06 23:48:27 +0800

    张三保存了test

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName Test
+ * @Description
+ * @Author Administrator
+ * @Date 23:46 2019/4/6 0006
+ * @Version 2.1
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("保存了test");
+    }
+}
